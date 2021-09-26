4370c67cdee44105af8fd19998493c5f523cba4f
MassimilianoMarcello <massimarcello@hotmail.it>
2021-09-26 23:07:52 +0200

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import image from "../Blue_particles.jpg"
 function Home() {
     return (
-        <div>
-            
-        </div>
+        <main>
+            <img src={image} alt="Blue particles" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Mass Marcello</h1>
+            </section>
+        </main>
     )
 }
 
